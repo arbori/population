37e1a3be82494b62e336e5695bc36b0d42143a93
arbori <marcelo.arbori@gmail.com>
2022-07-17 02:04:00 +0100

    The algorithm for simple version simulation has been implemented.

--- population.go
+++ population.go
@@ -3,168 +3,90 @@ package main
 import (
 	"fmt"
 	"math/rand"
-
-	"github.com/arbori/population.git/population/agent"
-	"github.com/arbori/population.git/population/space"
 )
 
-func interationRuleDefinition(a1 *agent.MobileAgent, a2 *agent.MobileAgent, contribuitionProbability float32, exchangeRate float32) {
-	if a1.Position[0] != a2.Position[0] || a1.Position[1] != a2.Position[1] {
-		return
-	}
+// Simulate I society surviving based in amount of energy each individuo consume (c),
+// how much energy individuos exchange (e), the probability of an individuo be salidary
+// with other with less energy when exchange energy (s) and tha amount of individuos
+// the society need to have to be viable (n).
+func SimulatedSociety(I []float32, c float32, e float32, s float32, n int) int {
+	var iterations int
 
-	selfishness := rand.Float32()
+	// Run the simulation while the society is viable.
+	for iterations = 0; len(I) > n; iterations += 1 {
+		P := make([]float32, 0, len(I))
 
-	if a1.Resources > a2.Resources {
-		if selfishness > contribuitionProbability {
-			a1.Resources = a1.Resources + exchangeRate*a2.Resources
-			a2.Resources = (1 - exchangeRate) * a2.Resources
-		} else {
-			a2.Resources = a2.Resources + exchangeRate*a1.Resources
-			a1.Resources = (1 - exchangeRate) * a1.Resources
-		}
-	} else {
-		if selfishness > contribuitionProbability {
-			a2.Resources = a2.Resources + exchangeRate*a1.Resources
-			a1.Resources = (1 - exchangeRate) * a1.Resources
-		} else {
-			a1.Resources = a1.Resources + exchangeRate*a2.Resources
-			a2.Resources = (1 - exchangeRate) * a2.Resources
-		}
-	}
-}
+		// Make exchange while more than two individous did not exchange yet.
+		for len(I) >= 2 {
+			ra := rand.Intn(len(I))
+			rb := rand.Intn(len(I))
 
-func motionRuleDefinition(environment *space.Environment, position *space.Point) space.Point {
-	neighborhood := environment.NeighborhoodValues((*position)[0], (*position)[1])
+			// The individuos must to be diferent
+			for ra == rb {
+				rb = rand.Intn(len(I))
+			}
 
-	var maxPosition int = 0
-	var maxValue float32 = neighborhood[maxPosition]
+			// Make exchange between ra and rb.
+			solidarity := rand.Float32()
 
-	for i := 1; i < len(neighborhood); i += 1 {
-		if neighborhood[i] > maxValue {
-			maxPosition = i
-			maxValue = neighborhood[i]
-		}
-	}
+			if (s < solidarity && I[ra] > I[rb]) || (s > solidarity && I[ra] < I[rb]) {
+				I[ra] += e
+				I[rb] -= e
+			} else if (s > solidarity && I[ra] > I[rb]) || (s < solidarity && I[ra] < I[rb]) {
+				I[ra] -= e
+				I[rb] += e
+			}
 
-	return environment.GetNewPosition(position, maxPosition)
-}
+			// Save the values.
+			P = append(P, I[ra], I[rb])
 
-var vonNeumannNeighborhoodMotion = space.NeighborhoodMotion{
-	Size: 5,
-	Directions: []space.Point{
-		[]int{0, 0},
-		[]int{-1, 0},
-		[]int{0, 1},
-		[]int{1, 0},
-		[]int{0, -1},
-	},
-}
-
-func constructMooreNeighborhoodMotion() space.NeighborhoodMotion {
-	motion := space.MakeNeighborhoodMotion(9, 2)
-
-	motion.Directions[0] = []int{0, 0}
-	motion.Directions[1] = []int{-1, 0}
-	motion.Directions[2] = []int{-1, 1}
-	motion.Directions[3] = []int{0, 1}
-	motion.Directions[4] = []int{1, 1}
-	motion.Directions[5] = []int{1, 0}
-	motion.Directions[6] = []int{1, -1}
-	motion.Directions[7] = []int{0, -1}
-	motion.Directions[8] = []int{-1, -1}
-
-	return motion
-}
-
-func constructEnvironment(motion *space.NeighborhoodMotion) space.Environment {
-	environment := space.MakeEnvironment(5, 5, motion, .05)
-
-	return environment
-}
-
-func exchangeBetweenAgents(agents []agent.MobileAgent, env *space.Environment) {
-
-}
-
-func showAgentsInEnvironment(environment space.Environment) {
-	for y := 0; y < environment.Y; y += 1 {
-		for x := 0; x < environment.X; x += 1 {
-			if environment.Cells[x][y].Content != nil && environment.Cells[x][y].Content.(*agent.MobileAgent).Resources > .01 {
-				fmt.Printf("%.2f\t", environment.Cells[x][y].Content.(*agent.MobileAgent).Resources) //"x\t")
+			// Remove from population individuos that already exchanges energy.
+			if ra < rb {
+				I = append(append(I[:ra], I[ra+1:rb]...), I[rb+1:]...)
 			} else {
-				fmt.Printf("_\t")
+				I = append(append(I[:rb], I[rb+1:ra]...), I[ra+1:]...)
 			}
 		}
 
-		fmt.Print("\n")
+		// Set the population for next iteration
+		if len(I) > 0 {
+			P = append(P, I...)
+		}
+
+		I = P
+
+		// Remove inviable individuos
+		for i := 0; i < len(I); i += 1 {
+			I[i] -= c
+
+			if I[i] < c {
+				I = append(I[:i], I[i+1:]...)
+			}
+		}
 	}
+
+	return iterations
 }
 
 func main() {
-	/*
-		motion := vonNeumannNeighborhoodMotion
-		environment := constructEnvironment(&motion)
+	fmt.Println("Simulated society")
 
-		spreadRule := rule.AverageRuleVonNeumann{}
+	I := make([]float32, 100)
+	var c float32 = 2000
+	var e float32 = c
+	var s float32 = .12
+	var n int = 2
 
-		size := 3
+	// Sortear a quantidade de recursos para os indivíduos de I.
+	for i := range I {
+		I[i] = 100.0 * c * rand.Float32()
+	}
 
-		agents := make([]*agent.MobileAgent, size, size)
-		agents[0] = &agent.MobileAgent{
-			IsAvailable: true,
-			Position:    []int {1, 1},
-			Resources:   7.0,
-			MotionRule:  motionRuleDefinition,
-		}
-		agents[1] = &agent.MobileAgent{
-			IsAvailable: true,
-			Position:    []int {3, 3},
-			Resources:   5.0,
-			MotionRule:  motionRuleDefinition,
-		}
-		agents[2] = &agent.MobileAgent{
-			IsAvailable: true,
-			Position:    []int {0, 4},
-			Resources:   6.0,
-			MotionRule:  motionRuleDefinition,
-		}
+	fmt.Printf("\nsolidarity\texistent\n")
 
-		dead := make([]*agent.MobileAgent, size, size)
+	for s = .0; s <= 1; s += .01 {
+		existent := SimulatedSociety(I, c, e, s, n)
 
-		for a := 0; a < len(agents); a += 1 {
-			environment.Cells[agents[a].Position[0]][agents[a].Position[1]].Content = agents[a]
-			environment.Cells[agents[a].Position[0]][agents[a].Position[1]].Value = agents[a].Resources
-		}
-
-		for t := 0; len(agents) > 0; t += 1 {
-			fmt.Printf("%d\n", t)
-			showAgentsInEnvironment(environment)
-			fmt.Print("\n\n")
-
-			environment.ApplyRule(spreadRule)
-
-			for a := 0; a < len(agents); a += 1 {
-				agents[a].Walk(&environment)
-
-				agents[a].Resources *= (1 - environment.Inertia)
-
-				if agents[a].Resources <= .01 {
-					dead = append(dead, agents[a])
-					agents = append(agents[:a], agents[a+1:]...)
-				}
-			}
-		}
-
-		for y := 0; y < environment.Y; y += 1 {
-			for x := 0; x < environment.X; x += 1 {
-				fmt.Printf("%.2f", environment.Cells[x][y].Value)
-				fmt.Print("\t")
-			}
-
-			fmt.Print("\n")
-		}
-
-		fmt.Print("\n\n")
-	*/
+		fmt.Printf("%f\t%d\n", s, existent)
+	}
 }
